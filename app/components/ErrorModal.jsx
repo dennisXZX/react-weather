ec041508d2887f024122692d8d6328cbfe509cdb
Dennis Xiao <dennisboys@gmail.com>
2017-05-23 15:21:38 +1000

    added comments to modal component

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -6,6 +6,7 @@ class ErrorModal extends Component {
 	componentDidMount() {
 		const { errorMessage } = this.props;
 
+		// store the HTML markup in a variable
 		const modalMarkup = (
 			<div id="error-modal" className="reveal tiny text-center" data-reveal="">
 				<h4>Error</h4>
@@ -16,14 +17,22 @@ class ErrorModal extends Component {
 			</div>			
 		);
 
+		// ReactDOMServer class is used to render components on the server
+		// ReactDOMServer.renderToString renders the React element to its initial HTML
 		const $modal = $(ReactDOMServer.renderToString(modalMarkup));
+		// ReactDOM.findDOMNode(component) returns the corresponding native browser DOM element
+		// convert the HTML to a jQuery object and change its HTML contents
 		$(ReactDOM.findDOMNode(this)).html($modal);
 
+		// create a Foundation Reveal instance
 		const modal = new Foundation.Reveal($('#error-modal'));
+		// open the modal
 		modal.open();
 	}
 
 	render() {
+		// the reason this modal component cannot be rendered in render() method is because
+		// Foundation manipulates the DOM directly which React is frowned upon
 		return (
 			<div></div>
 		)
